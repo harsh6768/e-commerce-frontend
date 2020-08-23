60d8f59f03686b31de3286d85bc255814cb629d5
Harsh Chaurasiya <harshchaurasiya6768@gmail.com>
2020-08-23 18:09:05 +0530

    protected routes functionality added

--- src/Modules/Home/Home.js
+++ src/Modules/Home/Home.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                Hello from Home
-            </div>
-        )
-    }
+
+	constructor(props) {
+        super(props);
+		this.state = {
+			isAuthenticated: false,
+		};
+	}
+
+	componentWillMount() {
+		let token = localStorage.getItem('token');
+		if (token != null && token != undefined) {
+			this.setState({
+				isAuthenticated: true,
+			});
+		}
+	}
+
+	render() {
+		const Component = this.props.component;
+        const { isAuthenticated } = this.state;
+        // Protected Routes
+		return isAuthenticated ? <h1>Dashboard Page</h1>  : <Redirect to={{ pathname: '/login' }} />;
+	}
 }
 
 export default Home;
--- src/Modules/Login/Login.js
+++ src/Modules/Login/Login.js
@@ -1,75 +1,102 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Login.css';
 
 // import LoginImage from '../../Images/login.svg';
 // import LoginImage from '../../Images/login1.svg';
 import LoginImage from '../../Images/login2.svg';
-import {GoogleOutlined} from '@ant-design/icons';
+import { GoogleOutlined } from '@ant-design/icons';
 
 class Login extends Component {
-   
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state={
-			email:'',
-			password:''
-		}
+		this.state = {
+			email: '',
+			password: '',
+		};
 	}
-   onHandleChange=(e)=>{
-	   const {name,value}=e.target;
-	  console.log(`Name : ${name} >>>> Value : ${value}`)
-	   this.setState({
-		   [name]:value,
-	   })
+	onHandleChange = (e) => {
+		const { name, value } = e.target;
+		console.log(`Name : ${name} >>>> Value : ${value}`);
+		this.setState({
+			[name]: value,
+		});
+	};
 
-   }
-   onFormSubmit=async(e)=>{
+	onFormSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const { email, password } = this.state;
+			console.log(`Email: ${email} >>>> Password : ${password}`);
 
-	 e.preventDefault();
+			let url = 'http://localhost:3001/login';
+			let response = await axios.post(url, {
+				email,
+				password,
+			});
 
-	 try{
-	   const {email,password}=this.state;
-	   console.log(`Email: ${email} >>>> Password : ${password}`)
+			console.log('Login Successful!');
+			console.log(response.data);
+			let { token, message } = response.data;
 
-	   let url='http://localhost:3001/login';
-	  await axios.post(url,{
-		   email,
-		   password
-	   })
+			localStorage.setItem('token', token);
 
-	 }catch(error){
-	   console.log(`Error : ${error}`)
-	   
-	 }
-   }
+			//pushing the routes
+			this.props.history.push({
+				pathname: '/',
+			});
+
+		} catch (error) {
+			console.log(`Error : ${error}`);
+		}
+	};
 	render() {
-		let {email,password}=this.state;
+		let { email, password } = this.state;
 		return (
 			<div>
 				<div className="login-page-container card  mx-auto d-flex  mt-5">
 					<div className="row card-body">
 						<div className="col-md-6">
-							<img src={LoginImage}  alt="SVG" className='img-fluid ' />
+							<img src={LoginImage} alt="SVG" className="img-fluid " />
 						</div>
 						<div className="col-md-6 mx-auto">
-							<p className='title'>Welcome to Nabula</p>		
-							<form method='POST' onSubmit={this.onFormSubmit}>
-							<div className='form-group'>
-							<input  className='form-control'  type="email" name='email' value={email} placeholder="Enter email" onChange={this.onHandleChange} />
-							</div>
-							<div className='form-group'>
-								<input  className='form-control' type="password" name='password' value={password} placeholder="Enter password" onChange={this.onHandleChange} />
-							</div>
-							<p className='forgot-password text-right'><Link to='/forget_password'>forgot password ?</Link></p>
-							<div className='mx-auto d-flex'>
-								<button className=" login-btn">Login</button>
-							</div>
-							</form>				
-							<div className='mx-auto d-flex mt-3'>
-								<span className='create-account'>New Nabula ?</span>
-								<span className='create-account text-primary'><Link to='/register' >&nbsp;Create Account</Link></span>
+							<p className="title">Welcome to Nabula</p>
+							<form method="POST" onSubmit={this.onFormSubmit}>
+								<div className="form-group">
+									<input
+										className="form-control"
+										type="email"
+										name="email"
+										value={email}
+										placeholder="Enter email"
+										onChange={this.onHandleChange}
+									/>
+								</div>
+								<div className="form-group">
+									<input
+										className="form-control"
+										type="password"
+										name="password"
+										value={password}
+										placeholder="Enter password"
+										onChange={this.onHandleChange}
+									/>
+								</div>
+								<p className="forgot-password text-right">
+									<Link to="/forget_password">forgot password ?</Link>
+								</p>
+								<div className="mx-auto d-flex">
+									<button type="submit" className=" login-btn">
+										Login
+									</button>
+								</div>
+							</form>
+							<div className="mx-auto d-flex mt-3">
+								<span className="create-account">New Nabula ?</span>
+								<span className="create-account text-primary">
+									<Link to="/register">&nbsp;Create Account</Link>
+								</span>
 							</div>
 							{/* <p className='text-center social-login '>Social Login</p>
 							<GoogleOutlined  style={{ fontSize: '16px', color:"blue"}} /> */}
